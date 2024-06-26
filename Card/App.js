2d43042b0500301020bb22728d3f58ead93a0b52
Nana-Yaw-Oteng <nyooteng@st.ug.edu.gh>
2024-06-26 12:17:11 +0000

    Added dark theme

--- Card/App.js
+++ Card/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from './components/HomeScreen';
 import MyCardsScreen from './components/MyCards';
-// import MyCardsScreen from './src/screens/MyCardsScreen';
-// import StatisticsScreen from './src/screens/StatisticsScreen';
-// import SettingsScreen from './src/screens/SettingsScreen';
+import StatisticsScreen from './components/StatisticsScreen';
+import SettingsScreen from './components/SettingsScreen';
+import { StyleSheet } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
   return (
-    <NavigationContainer>
+    
+    <NavigationContainer theme={isDarkTheme ? darkTheme : lightTheme}>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
@@ -30,18 +33,55 @@ const App = () => {
 
             return <Ionicons name={iconName} size={size} color={color} />;
           },
-          headerShown:false
+          headerShown: false,
         })}
-       
-        
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="My Cards" component={MyCardsScreen} />
-        {/* <Tab.Screen name="Statistics" component={StatisticsScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} /> */}
-      </Tab.Navigator>
+        <Tab.Screen name="Home">
+          {(props) => (
+            <HomeScreen
+              {...props}
+              isDarkTheme={isDarkTheme}
+            />
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="My Cards" component={MyCardsScreen}
+         />
+        <Tab.Screen name="Statistics" component={StatisticsScreen} />
+        <Tab.Screen name="Settings">
+          {(props) => (
+            <SettingsScreen
+              {...props}
+              isDarkTheme={isDarkTheme}
+              setIsDarkTheme={setIsDarkTheme}
+            />
+          )}
+        </Tab.Screen>
+      </Tab.Navigator >
     </NavigationContainer>
+    
   );
+};
+
+const lightTheme = {
+  dark: false,
+  colors: {
+    primary: 'rgb(26, 26, 255)',
+    background: 'white',
+    card: 'white',
+    text: 'black',
+    border: 'gray',
+  },
+};
+
+const darkTheme = {
+  dark: true,
+  colors: {
+    primary: 'rgb(26, 26, 255)',
+    background: '#000',
+    card: 'rgb(0, 0, 51)',
+    text: 'white',
+    border: 'gray',
+  },
 };
 
 export default App;
