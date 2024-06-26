7b411176088da753abf343892be6382c38caed76
Nana-Yaw-Oteng <nyooteng@st.ug.edu.gh>
2024-06-26 12:51:36 +0000

    Modified Cards component

--- Card/components/MyCards.js
+++ Card/components/MyCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const MyCardsScreen = () => {
+const MyCardsScreen = ({isDarkTheme}) => {
   return (
     <View style={styles.container}>
       <Text>My Cards Screen</Text>
